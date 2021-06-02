c6567d6530447938f679d292c484e5b83129ed8a
yanagawawa <yukkebbnba@gmail.com>
2021-06-02 00:10:19 +0000

    エラー文追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,9 +28,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] ="Book was successfully updated."
+       redirect_to book_path(book.id)
+    end
   end
 
   def destroy
