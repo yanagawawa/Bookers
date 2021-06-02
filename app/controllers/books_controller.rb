1aa55faa247f080df5aaa1ed5aae5c1a75d43f44
yanagawawa <yukkebbnba@gmail.com>
2021-06-02 00:45:51 +0000

    Update　エラー文の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
      flash[:notice] = "Book was successfully created."
-     redirect_to book_path(book.id)
+     redirect_to book_path(@book.id)
     else
      @books = Book.all
      render action: :index
@@ -31,7 +31,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
        flash[:notice] ="Book was successfully updated."
-       redirect_to book_path(book.id)
+       redirect_to book_path(@book.id)
     end
   end
 
